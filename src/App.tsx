cd89796dd272cdd795211dcac8f5a35d35c2a4e8
佐藤優太 <satouyuuta@YS13-inc-M2-MacBook-Air.local>
2024-01-23 01:51:50 +0900

    github actions

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1 className="text-3xl text-red-500">Hello,e</h1>
       <div className="card">
         <Button onClick={() => setCount((count) => count + 1)}>
-          count is a {count}
+          count is aa {count}
         </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
